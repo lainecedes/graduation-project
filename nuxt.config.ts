7a937105aab0b614ef9aa210a5e774b00d2d0140
lainecedes <elaine.parieh@gmail.com>
2025-12-04 14:48:09 +0100

    Deployment issues

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    app: {
-        baseURL: '/v1/',
-        buildAssetsDir: '/v1/_nuxt/'
-    },
     compatibilityDate: '2025-07-15',
     devtools: { enabled: true },
     vite: {
